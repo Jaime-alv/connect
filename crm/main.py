001d35213d9576fd58e624714fccbedf293457a0
Jaime Álvarez <88266316+Jaime-alv@users.noreply.github.com>
2021-10-15 17:08:24 +0200

    New error function and template

--- crm/main.py
+++ crm/main.py
@@ -29,20 +29,20 @@ def sign_up_form():
     fields = ['email', 'password', 'password_confirm']
     # check if all fields are complete -> form validation
     for field in fields:
-        value = flask.request.form.get(field, None)
-        if value is None or value == '':
+        value = flask.request.form.get(field)
+        if value == '':
             missing_field.append(field)
     if missing_field:
-        return flask.render_template('sign up error.html', inputs=missing_field, next=flask.url_for('sign_up'))
+        return error(f'Missing inputs in {missing_field}', 'sign_up')
     # check if email is already registered
     new_user = flask.request.form.get('email')
     if pathlib.Path(f'..\\user\\{new_user}').exists():
-        print('Email exists')
+        return error('User already exits', 'sign_up')
     # check if email is valid
     email_regex = re.compile(r"[a-zA-Z0-9_.]+@[a-zA-Z0-9_.+]+")
     email = email_regex.search(new_user)
     if email is None:
-        print('no valid email')
+        return error('email is not valid', 'sign_up')
     # check if password is valid
     password = flask.request.form.get('password')
     password_confirm = flask.request.form.get('password_confirm')
@@ -51,6 +51,13 @@ def sign_up_form():
             and any(character.isdigit() for character in password)
             and password == password_confirm):
         return flask.render_template('profile.html')
+    else:
+        return error('Password needs at least 1 upper, 1 digit and 1 punctuation', 'index')
+
+
+# generic error message, redirect to 'next_url'
+def error(message, next_url):
+    return flask.render_template('error.html', error_message=message, next=flask.url_for(next_url))
 
 
 if __name__ == '__main__':
