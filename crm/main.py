2a8d75c24c4ab3818390a24bcbb009f37c5ce248
Jaime Álvarez <88266316+Jaime-alv@users.noreply.github.com>
2021-10-15 18:05:37 +0200

    Create new user

--- crm/main.py
+++ crm/main.py
@@ -26,7 +26,8 @@ def sign_up():
 @app.route('/sign_up_form', methods=['POST'])
 def sign_up_form():
     missing_field = []
-    fields = ['email', 'password', 'password_confirm']
+    fields = ['organization', 'email', 'password', 'password_confirm']
+
     # check if all fields are complete -> form validation
     for field in fields:
         value = flask.request.form.get(field)
@@ -34,15 +35,19 @@ def sign_up_form():
             missing_field.append(field)
     if missing_field:
         return error(f'Missing inputs in {missing_field}', 'sign_up')
+
     # check if email is already registered
     new_user = flask.request.form.get('email')
-    if pathlib.Path(f'..\\user\\{new_user}').exists():
+    organization = flask.request.form.get('organization')
+    if pathlib.Path(f'..\\user\\{organization}\\{new_user}').exists():
         return error('User already exits', 'sign_up')
+
     # check if email is valid
     email_regex = re.compile(r"[a-zA-Z0-9_.]+@[a-zA-Z0-9_.+]+")
     email = email_regex.search(new_user)
     if email is None:
         return error('email is not valid', 'sign_up')
+
     # check if password is valid
     password = flask.request.form.get('password')
     password_confirm = flask.request.form.get('password_confirm')
@@ -50,9 +55,24 @@ def sign_up_form():
             and any(character.isupper() for character in password)
             and any(character.isdigit() for character in password)
             and password == password_confirm):
+        create_new_user(organization, new_user, password)
         return flask.render_template('profile.html')
     else:
         return error('Password needs at least 1 upper, 1 digit and 1 punctuation', 'index')
+
+
+# create user folder and json file with all data
+def create_new_user(organization, email, password):
+    if not pathlib.Path(f'..\\user\\{organization}').exists():
+        pathlib.Path(f'..\\user\\{organization}').mkdir(exist_ok=True)
+    data = {'organization': organization,
+            'user': email,
+            'password': password,
+            'messages': {},
+            'friends': [],
+            }
+    with pathlib.Path(f'..\\user\\{organization}\\{email}.txt').open('w') as f:
+        json.dump(data, f)
 
 
 # generic error message, redirect to 'next_url'
