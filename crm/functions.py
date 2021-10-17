f40d74099d36b6d99989b1ea11b953ae4e8296f2
Jaime Álvarez <88266316+Jaime-alv@users.noreply.github.com>
2021-10-17 19:31:15 +0200

    New file functions.py with only functions

---
+++ crm/functions.py
@@ -0,0 +1,46 @@
+import re
+import json
+import pathlib
+import flask
+from flask import Flask
+
+
+# load profile user
+def show_profile(user_email):
+    with pathlib.Path(f'..\\data\\user\\{user_email}\\user_profile.txt').open('r+') as file:
+        user_profile = json.load(file)
+        user = user_profile['user']
+        password = user_profile['password']
+        organization = user_profile['organization']
+    return flask.render_template('profile.html', user=user, password=password, organization=organization)
+
+
+# generic error message, redirect to 'next_url'
+def error(message, next_url):
+    return flask.render_template('error.html', error_message=message, next=flask.url_for(next_url))
+
+
+# create user folder and json file with all data
+def create_new_user(organization, email, password):
+    # create organization and add user as admin
+    if organization != '' and not pathlib.Path(f'..\\data\\inc\\{organization}').exists():
+        pathlib.Path(f'..\\data\\inc\\{organization}').mkdir(parents=True, exist_ok=True)
+        data_inc = {'admin': [],
+                    'employees': [],
+                    'clients': [],
+                    'client_data': {}}
+        with pathlib.Path(f'..\\data\\inc\\{organization}\\inc_profile.txt').open('w') as w:
+            data_inc['admin'].append(email)
+            data_inc['employees'].append(email)
+            json.dump(data_inc, w)
+
+    # create user profile
+    pathlib.Path(f'..\\data\\user\\{email}').mkdir(parents=True)
+    data_user = {'organization': organization,
+                 'user': email,
+                 'password': password,
+                 'messages': {},
+                 'contacts': [],
+                 }
+    with pathlib.Path(f'..\\data\\user\\{email}\\user_profile.txt').open('w') as f:
+        json.dump(data_user, f)
