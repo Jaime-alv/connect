3b3faa2a40a47f7615f1ef243861f7f311fa85cb
Jaime Álvarez <88266316+Jaime-alv@users.noreply.github.com>
2021-12-02 23:24:47 +0100

    Add sign in and log out functions

--- connect/routing.py
+++ connect/routing.py
@@ -6,18 +6,15 @@ from connect import forms
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Jaime'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return flask.render_template('index.html', user=user, posts=posts, title=user['username'])
+    return flask.render_template('index.html', title='Welcome to Connect!')
+
+
+@app.route('/sign_in', methods=['GET', 'POST'])
+def sign_in():
+    form = forms.SignInForm()
+    if form.validate_on_submit():
+        return flask.redirect(flask.url_for('profile'))
+    return flask.render_template('sign_in.html', title='Sign in', form=form)
 
 
 @app.route('/profile')
@@ -25,7 +22,7 @@ def profile():
     user = {'username': 'Jaime',
             'email': 'jaime@socnet',
             'friends': ['John', 'Susan']}
-    return flask.render_template('profile.html', user=user, title=user['username'])
+    return flask.render_template('profile.html', user=user, title='profile')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,5 +30,11 @@ def login():
     form = forms.LoginForm()
     if form.validate_on_submit():
         flask.flash(f'Login requested for user {form.username.data}, remember_me = {form.remember_me.data}')
-        return flask.redirect(flask.url_for('index'))
-    return flask.render_template('login.html', title='Sign in', form=form)
+        return flask.redirect(flask.url_for('profile'))
+    return flask.render_template('login.html', title='Log In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    flask.session.pop('user', None)
+    return flask.redirect(flask.url_for('index'))
