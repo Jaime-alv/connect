b7e58f75e7a81d13c85b6e8236a7bc811a08c87d
Jaime Álvarez <88266316+Jaime-alv@users.noreply.github.com>
2021-12-02 23:23:56 +0100

    Add SignInForm class

--- connect/forms.py
+++ connect/forms.py
@@ -7,3 +7,17 @@ class LoginForm(FlaskForm):
     password = wtforms.PasswordField('Password', validators=[wtforms.validators.DataRequired()])
     remember_me = wtforms.BooleanField('Remember me')
     submit = wtforms.SubmitField('Sign In')
+
+
+class SignInForm(FlaskForm):
+    username = wtforms.StringField('Username', validators=[wtforms.validators.DataRequired()])
+    user_email = wtforms.StringField('Email', validators=[wtforms.validators.DataRequired()])
+    password = wtforms.PasswordField('Password',
+                                     validators=[wtforms.validators.DataRequired(),
+                                                 wtforms.validators.EqualTo('repeat_password',
+                                                                            message='Passwords must match.')])
+    repeat_password = wtforms.PasswordField('Repeat Password',
+                                            validators=[wtforms.validators.DataRequired(),
+                                                        wtforms.validators.EqualTo('password',
+                                                                                   message='Passwords must match.')])
+    submit = wtforms.SubmitField('Sign In')
