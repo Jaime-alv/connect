cb18214a7cafc280d7c31f0afdca464305f24f68
Jaime Álvarez <88266316+Jaime-alv@users.noreply.github.com>
2021-12-02 23:23:32 +0100

    Add sentinel line

--- connect.py
+++ connect.py
@@ -1,4 +1,8 @@
 from connect import app
+import sys
 
 if __name__ == '__main__':
-    app.run()
+    if sys.platform == 'darwin':  # different port if running on MacOsX
+        app.run(port=8080)
+    else:
+        app.run(port=80)
